2a5023e16ca574bfcffb75ae4463ed779d57595d
NiushaH <35556570+NiushaH@users.noreply.github.com>
2022-09-11 17:04:05 -0600

    lab complete

--- index.js
+++ index.js
@@ -11,3 +11,14 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(timeString) {
+  const hour = parseInt(timeString, 10);
+  if (hour < 12) return "Good morning"
+  else if (hour > 17) return "Good Evening"
+  return "Good Afternoon"
+}
+
+function displayMessage() {
+  document.getElementById("greeting").textContent = msg;
+}
